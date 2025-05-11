232befa15ea938ab70a96b50839a6e08865c4a28
42shotfirst <morgan@morgankreed.com>
2025-05-11 06:39:06 +0000

    5/10/25 11:38p

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import emailjs from "@emailjs/browser";
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
 const basename = import.meta.env.BASE_URL;
+
+// Use the environment variable instead of the hardcoded string
+emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
